8d05012c449d345978add5fbe28bf00e66156592
Gilad Bison <gilad@appdome.com>
2019-07-14 22:33:38 +0300

    added authentication

--- app/src/main/java/com/example/badger/MainActivity.java
+++ app/src/main/java/com/example/badger/MainActivity.java
@@ -6,8 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +23,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void Login(View view) {
-        Intent intent = new Intent(this, HomePageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra("gilad", message);
-        startActivity(intent);
+//    public void Login(View view) {
+//        Intent intent = new Intent(this, HomePageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra("gilad", message);
+//        startActivity(intent);
+//    }
+
+    public void signIn(View view) {
+        startActivityForResult(
+                // Get an instance of AuthUI based on the default app
+                AuthUI.getInstance().createSignInIntentBuilder().build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser fbUser = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(this, "Authenticated as " + fbUser.getDisplayName(), Toast.LENGTH_SHORT).show();
+            } else {
+                // Sign in failed, check response for error code
+                if (response != null) {
+                    Toast.makeText(this, response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
