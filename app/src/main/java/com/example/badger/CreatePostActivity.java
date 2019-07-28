100d84f88413a700b7098ce7565d283e1c1134b3
Gilad Bison <gilad@appdome.com>
2019-07-28 20:11:30 +0300

    Added Post object, and upload it when picking a photo

--- app/src/main/java/com/example/badger/CreatePostActivity.java
+++ app/src/main/java/com/example/badger/CreatePostActivity.java
@@ -7,21 +7,35 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class CreatePostActivity extends AppCompatActivity {
 
     private Uri imageURI;
+    DatabaseReference database;
+    FirebaseUser fbUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,41 +45,64 @@ public class CreatePostActivity extends AppCompatActivity {
         this.imageURI = Uri.parse(callerIntent.getStringExtra("imageUri"));
         ImageView previewImageView = findViewById(R.id.previewImage);
         previewImageView.setImageURI(this.imageURI);
+        fbUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (fbUser == null) {
+            finish();
+        }
+
+        database = FirebaseDatabase.getInstance().getReference();
+    }
+
+
+    private Post getPostFromUI() {
+        EditText descriptionEditText = findViewById(R.id.editText);
+        String description = descriptionEditText.getText().toString();
+        ArrayList<String> badges = new ArrayList<String>();
+
+        ChipGroup badgesChipGroup = findViewById(R.id.filter_chip_group);
+        for (int i = 0; i < badgesChipGroup.getChildCount(); i++) {
+            Chip currentChip  = (Chip)badgesChipGroup.getChildAt(i);
+            if (currentChip.isChecked()) {
+                badges.add(currentChip.getText().toString());
+            }
+        }
+
+        String key = database.child("posts").push().getKey();
+        return new Post(key, fbUser.getUid(), this.imageURI.toString(), description, badges);
     }
 
     public void sendPost(View view) {
+          final Post postToUpload = this.getPostFromUI();
 //        Uri uri = data.getData();
 //
-//        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
-//        StorageReference imagesRef = storageRef.child("images");
-//        StorageReference userRef = imagesRef.child(fbUser.getUid());
-//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-//        String filename = fbUser.getUid() + "_" + timeStamp;
-//        final StorageReference fileRef = userRef.child(filename);
-//
-//        UploadTask uploadTask = fileRef.putFile(uri);
-//        uploadTask.addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception exception) {
-//                // Handle unsuccessful uploads
-//                Toast.makeText(HomePageActivity.this, "Upload failed!\n" + exception.getMessage(), Toast.LENGTH_LONG).show();
-//            }
-//        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-//            @Override
-//            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-//                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-//                    @Override
-//                    public void onSuccess(Uri uri) {
-//                        Uri downloadUrl = uri;
-//                        Toast.makeText(HomePageActivity.this, "Upload finished!", Toast.LENGTH_SHORT).show();
-//                        // save image to database
-//                        String key = database.child("images").push().getKey();
-//                        Image image = new Image(key, fbUser.getUid(), downloadUrl.toString());
-//                        database.child("images").child(key).setValue(image);
-//                    }
-//                });
-//
-//            }
-//        });
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference imagesRef = storageRef.child("images");
+        StorageReference userRef = imagesRef.child(fbUser.getUid());
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String filename = fbUser.getUid() + "_" + timeStamp;
+        final StorageReference fileRef = userRef.child(filename);
+
+        UploadTask uploadTask = fileRef.putFile(this.imageURI);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+                Toast.makeText(CreatePostActivity.this, "Upload failed!\n" + exception.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Uri downloadUrl = uri;
+                        Toast.makeText(CreatePostActivity.this, "Upload finished!", Toast.LENGTH_SHORT).show();
+                        // save image to database
+                        database.child("posts").child(postToUpload.key).setValue(postToUpload);
+                    }
+                });
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/badger/Post.java
@@ -0,0 +1,48 @@
+package com.example.badger;
+
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.ArrayList;
+
+@IgnoreExtraProperties
+public class Post {
+    public String key;
+    public String userId;
+    public String imageDownloadUrl;
+    public String description;
+    public ArrayList<String> badges;
+
+    // these properties will not be saved to the database
+    @Exclude
+    public User user;
+
+    @Exclude
+    public int likes = 0;
+
+    @Exclude
+    public boolean hasLiked = false;
+
+    @Exclude
+    public String userLike;
+
+    public Post() {
+        // Default constructor required for calls to DataSnapshot.getValue(com.example.badger.User.class)
+    }
+
+    public Post(String key, String userId, String imageDownloadUrl, String description, ArrayList<String> badges) {
+        this.key = key;
+        this.userId = userId;
+        this.imageDownloadUrl = imageDownloadUrl;
+        this.description = description;
+        this.badges = badges;
+    }
+
+    public void addLike() {
+        this.likes++;
+    }
+
+    public void removeLike() {
+        this.likes--;
+    }
+}
